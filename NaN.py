84cb0cc5dfb35e5303d6e68ee6a12626b2be70eb
indianalifb <Indiana.lif@gmail.com>
2019-12-02 15:12:50 +0000

    Update NaN.py

--- NaN.py
+++ NaN.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class IOAPI():
     def __init__(self, planeID):
         self.planeID = planeID
@@ -25,3 +28,7 @@ class IOAPI():
         return fileStream_crew
 
     def storeCrewToFile(self, newstaff):
+        with open("Crew.csv", "a" "\n") as file:
+
+
+a = 'Hello world'
