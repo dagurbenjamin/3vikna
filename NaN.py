b5105d99917096b4f7893b007dffb883e5527532
indianalifb <Indiana.lif@gmail.com>
2019-12-02 15:52:31 +0000

    Update NaN.py

--- NaN.py
+++ NaN.py
@@ -2,8 +2,8 @@ import csv
 
 
 class IOAPI():
-    def __init__(self, planeID):
-        self.planeID = planeID
+    def __init__(self, foo):
+        self.foo = foo
 
     def loadVoyageFromFile(self, voyageID):
         pass
@@ -27,18 +27,22 @@ class IOAPI():
         fileStream_crew = open("Crew.csv", "r")
         return fileStream_crew
 
-    def storeCrewToFile(self, newstaff):
+    def storeCrewToFile(self, foo):
         a = CrewIO(foo)
         a.write_in_file(foo)
 
 
 class CrewIO(IOAPI):
     def __init__(self, foo):
+        self.foo = foo
 
-    def write_in_file(self, foo)
-    f = open("CrewFile.csv", "a")
-    f.write(foo)
-    f.close()
+    def write_in_file(self, foo):
+        f = open("CrewFile.csv", "a")
+        f.write(foo)
+        f.close()
 
-    def lol():
-        pass
+
+def main():
+    foo = 'Hello world \n'
+    a = IOAPI(foo)
+    a.storeCrewToFile(foo)
