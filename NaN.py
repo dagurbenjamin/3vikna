e09fd673b4897c83cb9bf422b23c6c5076ca3c21
indianalifb <Indiana.lif@gmail.com>
2019-12-02 14:46:32 +0000

    Update NaN.py

--- NaN.py
+++ NaN.py
@@ -1,3 +1,27 @@
 class IOAPI():
     def __init__(self):
         pass
+
+    def loadVoyageFromFile(self, voyageID):
+        pass
+
+    def storeVoyageToFile(self):
+        pass
+
+    def loadDestinationFromFile(self):
+        pass
+
+    def storeDestinationToFile(self):
+        pass
+
+    def loadPlaneFromFile(self, planeID):
+        pass
+
+    def storePlaneToFile(self):
+        pass
+
+    def loadCrewFromFile(self, crewID):
+        pass
+
+    def storeCrewToFile(self):
+        pass
