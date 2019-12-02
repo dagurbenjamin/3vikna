51f4ad6cf12e28134121df508945ae8033d51d07
indianalifb <Indiana.lif@gmail.com>
2019-12-02 16:01:10 +0000

    Update profun.py

--- profun.py
+++ profun.py
@@ -6,7 +6,7 @@ class IOAPI():
         self.foo = foo
 
     def storeCrewToFile(self, foo):
-        f = open("CrewFile.csv", "a")
+        f = open("CrewFile.csv", "w+")
         f.write(foo)
         f.close()
 
@@ -14,7 +14,3 @@ class IOAPI():
 foo = 'Hello world \n'
 a = IOAPI(foo)
 a.storeCrewToFile(foo)
-
-
-# def main():
-#     boo = IOAPI(foo)
