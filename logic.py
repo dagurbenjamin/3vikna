6f455f6f140dd845c26c61f660e891a7fcd166c2
indianalifb <Indiana.lif@gmail.com>
2019-12-03 11:12:56 +0000

    Update logic.py

---
+++ logic.py
@@ -0,0 +1,23 @@
+class LLAPI():
+    def __init__(self):
+        pass
+
+
+class airplanesLL():
+    pass
+
+
+class destinationsLL():
+    pass
+
+
+class employeesLL():
+    pass
+
+
+class flights():
+    pass
+
+
+class voyagesLL():
+    pass
