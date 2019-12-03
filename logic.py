62a38c4ec9ef17ada1f47508bd3ccdc8ffbe5ad3
indianalifb <Indiana.lif@gmail.com>
2019-12-03 11:38:40 +0000

    :+1:

--- logic.py
+++ logic.py
@@ -1,23 +1,36 @@
+EMPLOYEENUMBER = 1
+
+
 class LLAPI():
     def __init__(self):
-        pass
 
 
-class airplanesLL():
+class AirplanesLL():
     pass
 
 
-class destinationsLL():
+class DestinationsLL():
     pass
 
 
-class employeesLL():
+class EmployeesLL():
+    def __init__(self, new_empl_list):
+        self.new_empl_str = new_empl_str
+
+    def create_ID(self, new_empl_list):
+        name = new_empl_list[1].split()
+
+
+class Flights():
     pass
 
 
-class flights():
+class VoyagesLL():
     pass
 
 
-class voyagesLL():
-    pass
+#ssn,name,role,rank,licence,address,phonenumber, email
+a_list = ['2910858778', 'Virginia Ho,Pilot', 'Captain',
+          'NABAE146', 'Fellsmuli 2', '8998802', 'virginia@NaN.is']
+a = EmployeesLL(a_list)
+a.create_ID(a_list)
--- profun_indi.py
+++ profun_indi.py
@@ -1,5 +1,21 @@
-import csv
-foo = open("UpcomingFlightsFile.csv", "r")
-reader = csv.DictReader(foo)
-for row in reader:
-    print(row)
+class EmployeesLL():
+    def __init__(self, new_empl_list):
+        self.new_empl_str = new_empl_str
+
+    def create_ID(self, new_empl_list):
+        name = new_empl_list[1].split()
+
+
+class Flights():
+    pass
+
+
+class VoyagesLL():
+    pass
+
+
+#ssn,name,role,rank,licence,address,phonenumber, email
+a_list = ['2910858778', 'Virginia Ho,Pilot', 'Captain',
+          'NABAE146', 'Fellsmuli 2', '8998802', 'virginia@NaN.is']
+a = EmployeesLL(a_list)
+a.create_ID(a_list)
