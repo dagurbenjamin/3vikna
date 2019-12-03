4c7b2458a978fbf5d2e49bbce33fbe716ee1ba2c
indianalifb <Indiana.lif@gmail.com>
2019-12-03 11:49:29 +0000

    :100:

--- profun_indi.py
+++ profun_indi.py
@@ -1,21 +1,14 @@
 class EmployeesLL():
     def __init__(self, new_empl_list):
-        self.new_empl_str = new_empl_str
+        self.new_empl_list = new_empl_list
 
     def create_ID(self, new_empl_list):
-        name = new_empl_list[1].split()
-
-
-class Flights():
-    pass
-
-
-class VoyagesLL():
-    pass
+        name = new_empl_list[1].split(' ')
+        print(new_empl_list)
 
 
 #ssn,name,role,rank,licence,address,phonenumber, email
-a_list = ['2910858778', 'Virginia Ho,Pilot', 'Captain',
+a_list = ['2910858778', 'Virginia Ho', 'Pilot', 'Captain',
           'NABAE146', 'Fellsmuli 2', '8998802', 'virginia@NaN.is']
 a = EmployeesLL(a_list)
 a.create_ID(a_list)
