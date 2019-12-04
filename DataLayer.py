0cab8e9cb5a4417168fbf06160dd7fdc3be6c843
indianalifb <Indiana.lif@gmail.com>
2019-12-04 12:59:24 +0000

    refactoring datalayer

--- DataLayer.py
+++ DataLayer.py
@@ -9,17 +9,15 @@ class IOAPI():
         fileStream_past_flights = open("PastFlightsFile.csv", "r")
         return fileStream_past_flights
 
-    def store_past_flights_to_file(self, a_str):
-        c = VoyagesIO(self.a_str)
-        c.write_in_file_past_flights(self.a_str)
+    def store_past_flights_to_file(self, new_past_flight):
+        c = VoyagesIO().write_in_file_past_flights(new_past_flight)
 
     def load_upcoming_flights_from_file(self):
         fileStream_upcoming_flights = open("UpcomingFlightsFile.csv", "r")
         return fileStream_upcoming_flights
 
-    def store_upcoming_flights_to_file(self, a_str):
-        d = VoyagesIO(self.a_str)
-        d.write_in_file_upcoming_flights(self.a_str)
+    def store_upcoming_flights_to_file(self, new_upcoming_flight):
+        d = VoyagesIO().write_in_file_upcoming_flights(new_upcoming_flight)
 
     def load_destination_from_file(self, destination):
         fileStream_destinations = open("DestinationsFile.csv", "r")
@@ -32,26 +30,23 @@ class IOAPI():
         fileStream_aircraft = open("Aircraft.csv", "r")
         return fileStream_aircraft
 
-    def store_airplanes_to_file(self, a_str):
-        g = AirplanesIO(self.a_str)
-        g.write_in_airplanes_file(self.a_str)
+    def store_airplanes_to_file(self, new_airplane):
+        g = AirplanesIO().write_in_airplanes_file(new_airplane)
 
     def load_airplanesinfo_from_file(self):
         fileStream_airplanesinfo = open("AirplanesInfoFile.csv", "r")
         return fileStream_airinfo
 
-    def store_airplanesinfo_to_file(self, a_str):
-        s = AirplanesIO(self.a_str)
-        s.write_in_airplanesinfo_file(self.a_str)
+    def store_airplanesinfo_to_file(self, new_airplane_type=''):
+        s = AirplanesIO().write_in_airplanesinfo_file(new_airplane_type)
 
     def load_crew_from_file(self):
-        #print('load_crew_from_file--inní falli')
         fileStream_crew = open("Crew.csv", "r")
         # ef empty hvað þá?
         return fileStream_crew
 
-    def store_crew_to_file(self, new_crew_member=''):
-        b = CrewIO().write_in_file(new_crew_member)
+    def store_crew_to_file(self, new_employee=''):
+        b = CrewIO().write_in_file(new_employee)
 
 
 class CrewIO(IOAPI):
@@ -68,14 +63,14 @@ class AirplanesIO(IOAPI):
     def __init__(self, a_str=''):
         self.a_str = a_str
 
-    def write_in_airplanes_file(self, a_str):
+    def write_in_airplanes_file(self, new_airplane):
         f = open("AirplanesFile.csv", "a")
-        f.write(a_str)
+        f.write(new_airplane)
         f.close()
 
-    def write_in_airplanesinfo_file(self, a_str):
+    def write_in_airplanesinfo_file(self, new_airplane_type):
         f = open("AirplanesInfoFile.csv", "a")
-        f.write(a_str)
+        f.write(new_airplane_type)
         f.close()
 
 
@@ -93,36 +88,32 @@ class VoyagesIO():
     def __init__(self, a_str):
         self.a_str = a_str
 
-    def write_in_file_past_flights(self, a_str):
+    def write_in_file_past_flights(self, new_past_flight):
         f = open("PastFlightsFile.csv", "a")
-        f.write(a_str)
+        f.write(new_past_flight)
         f.close()
 
-    def write_in_file_upcoming_flights(self, a_str):
+    def write_in_file_upcoming_flights(self, new_upcoming_flight):
         f = open("UpcomingFlightsFile.csv", "a")
-        f.write(a_str)
+        f.write(new_upcoming_flight)
         f.close()
 
 
 def main():
     newEmployee = 'New employee \n'
-    a = IOAPI().store_crew_to_file(newEmployee)
+    a = IOAPI().store_crew_to_file(new_employee)
 
     pastFlight = "i am a past flight \n"
-    b = IOAPI(pastFlight)
-    b.store_past_flights_to_file(pastFlight)
+    b = IOAPI().store_past_flights_to_file(new_past_flight)
 
     UpcomingFlight = "i am a upcoming flight \n"
-    y = IOAPI(UpcomingFlight)
-    y.store_upcoming_flights_to_file(UpcomingFlight)
+    y = IOAPI().store_upcoming_flights_to_file(new_upcoming_flight)
 
     airplaneinfo = "i am info \n"
-    y = IOAPI(airplaneinfo)
-    y.store_airplanesinfo_to_file(airplaneinfo)
+    y = IOAPI().store_airplanesinfo_to_file(new_airplane_type)
 
     airplane = "i am airplane \n"
-    j = IOAPI(airplane)
-    j.store_airplanes_to_file(airplane)
+    j = IOAPI().store_airplanes_to_file(new_airplane)
 
     newDestination = "i am a destination \n"
     j = IOAPI().store_destination_to_file(newDestination)
