f81a8ec156332256c247a552110af4dd487c9d82
indianalifb <Indiana.lif@gmail.com>
2019-12-05 17:07:58 +0000

    minor changes

--- logic/AirplanesLL.py
+++ logic/AirplanesLL.py
@@ -3,31 +3,21 @@ from LLAPI import LLAPI
 from IOLAYER.IOAPI import IOAPI
 
 
-
-class LLAPI():
-    def __init__(self, a_str=''):
-        self.a_str = a_str
-
-
 class AirplanesLL:
     def __init__(self, plane_list):
         self.plane_list = plane_list
-
 
     def create_airplane_info(self, plane_list):
         plane_list = ",".join(plane_list)
         return plane_list
 
-
     def new_airplane(self, new_plane):
         new_plane = ",".join(new_plane)
         return new_plane
 
-
     def get_airplanes(self, the_plains):
         the_plains = list(the_plains.split(", "))
         return the_plains
-
 
     def get_airplane_types(self, plane_types):
         title = 'planeTypeId,manufacturer,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan'
@@ -43,9 +33,9 @@ class AirplanesLL:
         return plane_types_dict
 
 
-
 def main():
-    plane_list = ["NABAE146", "BAE", "146", "82", "23820", "38101", "31.1", "11000", "26.19", "8.61", "26", "34"]
+    plane_list = ["NABAE146", "BAE", "146", "82", "23820",
+                  "38101", "31.1", "11000", "26.19", "8.61", "26", "34"]
     new_plane = ["ID", "Name"]
     the_plains = "ID, Name"
     plain_type = "NABAE146, BAE, 146, 82, 23820, 38101, 31.1, 11000, 26.19, 8.61, 26, 34"
@@ -57,4 +47,6 @@ def main():
     c.get_airplanes(the_plains)
     d = AirplanesLL(plain_type)
     d.get_airplane_types(plain_type)
+
+
 main()
