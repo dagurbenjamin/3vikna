52e36c992391aab751f92af16241737ecc3bc584
ivarhjalt <ivarhjalt@gmail.com>
2019-12-06 12:49:40 +0000

    modules

--- MODULES/aircraft.py
+++ MODULES/aircraft.py
@@ -2,13 +2,21 @@ class Aircraft:
 
     def __init__(self, insignia, typeid):
         self.insignia = insignia
-        self.type.id = typeid
+        self.typeid = typeid
     
-    def __str__(self):
-        return "{},{}".format(self.insignia, self.typeid)
+
 
     def get_insignia(self):
         return self.insignia
     
     def get_typeid(self):
         return self.get_typeid
+
+    def set_insignia(self, new):
+        self.insignia = new
+    
+    def set_typeid(self, new):
+        self.typeid = new
+        
+    def __str__(self):
+    return "{},{}".format(self.insignia, self.typeid)
--- MODULES/pastflights.py
+++ MODULES/pastflights.py
@@ -1,46 +1,80 @@
+class PastFlights():
 
 
-def __init__(self, departingFrom, arrivingAt, departure, arrival, aircraftID, captain, copilots, fsm, fa1, fa2):
-    self.departingFrom = departingFrom
-    self.arrivingAt = arrivingAt
-    self.departure = departure
-    self.arrival = arrival
-    self.aircraftID = aircraftID
-    self.captain = captain
-    self.copilot = copilot
-    self.fsm = fsm
-    self.fa1 = fa1
-    self.fa2 = fa2
+    def __init__(self, departingFrom, arrivingAt, departure, arrival, aircraftID, captain, copilot, fsm, fa1, fa2):
+        self.departingFrom = departingFrom
+        self.arrivingAt = arrivingAt
+        self.departure = departure
+        self.arrival = arrival
+        self.aircraftID = aircraftID
+        self.captain = captain
+        self.copilot = copilot
+        self.fsm = fsm
+        self.fa1 = fa1
+        self.fa2 = fa2
 
-def __str__(self):
-    return "{},{},{},{},{},{},{},{},{},{}".format(self.departingFrom, self.arrivingAt, self.departure, self.arrival, self.aircraftID, self.captain, self.copilots, self.fsm, self.fa1, self.fa2)
 
-def get_departingFrom(self):
-    return self.departingFrom
 
-def get_arrivingAt(self):
-    return self.arrivingAt
+    def get_departingFrom(self):
+        return self.departingFrom
 
-def get_departure(self):
-    return self.departure
+    def get_arrivingAt(self):
+        return self.arrivingAt
 
-def get_arrival(self):
-    return self.arrival
+    def get_departure(self):
+        return self.departure
 
-def get_aircraftID(self):
-    return self.aircraftID
+    def get_arrival(self):
+        return self.arrival
 
-def  get_captain(self):
-    return self.captain
+    def get_aircraftID(self):
+        return self.aircraftID
 
-def get_copilot(self):
-    return self.copilot
+    def  get_captain(self):
+        return self.captain
 
-def get_fsm(self):
-    return self.fsm
+    def get_copilot(self):
+        return self.copilot
 
-def get_fa1(self):
-    return self.fa1
+    def get_fsm(self):
+        return self.fsm
 
-def get_fa2(self):
-    return self.fa2
+    def get_fa1(self):
+        return self.fa1
+
+    def get_fa2(self):
+        return self.fa2
+
+    def set_departingFrom(self, new):
+        self.departingFrom = new
+
+    def set_arrivingAt(self, new):
+        self.arrivingAt = new
+
+    def set_departure(self, new):
+        self.departure = new
+
+    def set_arrival(self, new):
+        self.arrival = new
+
+    def set_aircraftID(self, new):
+        self.aircraftID = new
+
+    def set_captain(self, new):
+        self.captain = new
+
+    def set_copilot(self, new):
+        self.copilot = new
+
+    def set_fsm(self, new):
+        self.fsm = new  
+
+    def set_fa1(self, new):
+        self.fa1 = new  
+
+    def set_fa2(self, new):
+        self.fa2 = new  
+
+    def __str__(self):
+        return "{},{},{},{},{},{},{},{},{},{}".format(self.departingFrom, self.arrivingAt, self.departure, self.arrival, self.aircraftID,
+                                                      self.captain, self.copilot, self.fsm, self.fa1, self.fa2)
