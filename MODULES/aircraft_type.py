25ad6e83407d0feb6e0194651e2e9333edba0a38
ingaldis <58175417+ingaldis@users.noreply.github.com>
2019-12-06 14:04:04 +0000

    laga nöfn

--- MODULES/aircraft_type.py
+++ MODULES/aircraft_type.py
@@ -3,14 +3,14 @@
 class Aircraft_type:
 
     def __init__(self, TypeId, manufacturer, model, capacity, emptyWeight, maxtakeoffWeight, unitThrust, serviceCeiling, length, height, wingspan):
-        self.type = TypeId
+        self.ypeId = TypeId
         self.manufacturer = manufacturer
         self.model = model
         self.capacity = capacity
-        self.eweight = emptyWeight
-        self.fweight = maxTakeoffWeight
-        self.thrust = unitThrust
-        self.ceiling = serviceCeiling
+        self.emptyWeight = emptyWeight
+        self.maxTakeoffWeight = maxTakeoffWeight
+        self.unitThrust = unitThrust
+        self.serviceCeiling = serviceCeiling
         self.length = length
         self.height = height
         self.wingspan = wingspan
@@ -18,7 +18,7 @@ class Aircraft_type:
     def __str__(self):
         return "{},{},{},{},{},{},{},{},{},{},{}".format(self.TypeId, self.manufacturer, self.model, self.capacity
                                                         self.emptyWeight, self.maxTakeoffWeight, self.unitThrust, self.serviceCeiling,
-                                                        self.length, self.height, self.wingspan)
+                                                       self.length, self.height, self.wingspan)
 
     def get_typeid(self):  
          return self.get_TypeId
