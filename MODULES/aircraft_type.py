2831affface4ba6e1e43daaf9e36860f6c7eef39
summifu <dagurr19@ru.is>
2019-12-06 14:20:25 +0000

    Breyti breytum

--- MODULES/aircraft_type.py
+++ MODULES/aircraft_type.py
@@ -2,29 +2,29 @@
 
 class Aircraft_type:
 
-    def __init__(self, TypeId, manufacturer, model, capacity, emptyWeight, maxtakeoffWeight, unitThrust, serviceCeiling, length, height, wingspan):
-        self.ypeId = TypeId
+    def __init__(self, type_id, manufacturer, model, capacity, empty_weight, max_takeoff_weight, unit_thrust, service_ceiling, length, height, wingspan):
+        self.type_id = type_id
         self.manufacturer = manufacturer
         self.model = model
         self.capacity = capacity
-        self.emptyWeight = emptyWeight
-        self.maxTakeoffWeight = maxTakeoffWeight
-        self.unitThrust = unitThrust
-        self.serviceCeiling = serviceCeiling
+        self.empty_weight = empty_weight
+        self.max_takeoff_weight = max_takeoff_weight
+        self.unit_thrust = unit_thrust
+        self.service_ceiling = service_ceiling
         self.length = length
         self.height = height
         self.wingspan = wingspan
 
     def __str__(self):
-        return "{},{},{},{},{},{},{},{},{},{},{}".format(self.TypeId, self.manufacturer, self.model, self.capacity
-                                                        self.emptyWeight, self.maxTakeoffWeight, self.unitThrust, self.serviceCeiling,
-                                                        self.length, self.height, self.wingspan)
+        return "{},{},{},{},{},{},{},{},{},{},{}".format(self.type_id, self.manufacturer, self.model, self.capacity,
+                                                         self.empty_weight, self.max_takeoff_weight, self.unit_thrust,
+                                                         self.service_ceiling, self.length, self.height, self.wingspan)
     
     def __repr__(self):
         return self.__str__()
 
     def get_typeid(self):  
-         return self.get_TypeId
+         return self.get_typeid
 
     def get_manufacturer(self):
         return self.manufacturer
@@ -36,16 +36,16 @@ class Aircraft_type:
         return self.capacity
     
     def get_eweight(self):
-        return self.emptyWeight
+        return self.empty_weight
 
     def get_fweight(self):
-        return self.maxTakeoffWeight
+        return self.max_takeoff_weight
 
     def get_thrust(self):
-        return self.unitThrust
+        return self.unit_thrust
     
     def get_ceiling(self):
-        return self.serviceCeiling
+        return self.service_ceiling
     
     def get_length(self):
         return self.length
