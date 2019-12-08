ef8fb0a9c041e6e450a1db1f957abeedbe643c9e
ingaldis <58175417+ingaldis@users.noreply.github.com>
2019-12-08 16:55:34 +0000

    airplane test

--- test_inga.py
+++ test_inga.py
@@ -1,10 +1,28 @@
 import csv
 import os
-from modules.Crew.Crew import Crew
+from modules.aircraft.Aircraft import Aircraft
+from modules.aircraft_type.Aircraft_type import Aircraft_type
 
-class CrewData(object):
+
+class AircraftData(object):
     def __init__(self):
         pass
 
-    def check_if_kt_exist(self, kt):
-        data = self.get
+    @staticmethod
+    def get_aircraft():
+        try:
+            with open("./data_files/AircraftType.csv") as file:
+                csv_reader = csv.DictReader(file)
+                aircrafts = []
+                for line in csv_reader:
+                    aircrafts.append(line)
+                return aircrafts
+            except Exception:
+                return False
+        
+        @staticmethod
+        def add_aircraft(aircraft):
+            model = aircraft.get_model()
+            
+
+
