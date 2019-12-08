c0e49d2f0f42627b0ff7550e4b35dd4f6c7780ee
indianalifb <Indiana.lif@gmail.com>
2019-12-08 16:54:45 +0000

    made functions to get employees

--- iolayer/CrewIO.py
+++ iolayer/CrewIO.py
@@ -6,17 +6,32 @@ class CrewIO():
     def __init__(self, a_str=''):
         self.a_str = a_str
 
-    def load_crew_from_file(self):
+    def load_crew_from_file(self, ssn_toFind):
         allcrew = []
         with open('./data_files/CrewFile.csv') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                if (row[ssn] == ssn_toFind):
+                if ssn_toFind == '0':
+                    employee = Crew(row['ssn'], row['name'], row['role'], row['rank'], row['licence'],
+                                    row['address'], row['phonenumber'], row['email'], row['id'])
+                    allcrew.append(employee)
+                elif row['ssn'] == ssn_toFind:
                     employee = Crew(row['ssn'], row['name'], row['role'], row['rank'], row['licence'],
                                     row['address'], row['phonenumber'], row['email'], row['id'])
                     allcrew.append(employee)
                     break
         return allcrew  # samt bara 1
+
+    def load_pilot_or_cabincrew(self, p_or_c_input):
+        all_p_or_c = []
+        with open('./data_files/CrewFile.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if (row['role'] == p_or_c_input):
+                    employee = Crew(row['ssn'], row['name'], row['role'], row['rank'],
+                                    row['licence'], row['address'], row['phonenumber'], row['email'], row['id'])
+                    all_p_or_c.append(employee)
+        return all_p_or_c
 
     def write_in_file(self, new_crew_member=''):
         f = open("CrewFile.csv", "a")
--- profun_indi.py
+++ profun_indi.py
@@ -3,6 +3,7 @@ from iolayer.AirplanesIO import AirplanesIO
 from iolayer.CrewIO import CrewIO
 from iolayer.DestinationsIO import DestinationsIO
 from iolayer.VoyagesIO import VoyagesIO
+from modules.Crew import Crew
 
 #x = IOAPI().load_destination_from_file()
 # print('')
@@ -11,9 +12,25 @@ from iolayer.VoyagesIO import VoyagesIO
 # IOAPI().store_crew_to_file(c)
 
 
-x = AirplanesIO().load_airplanes_from_file()
-y = AirplanesIO().load_airplanesinfo_from_file()
-a = CrewIO().load_crew_from_file()
-b = DestinationsIO().load_destination_from_file()
-c = VoyagesIO().load_past_flights_from_file()
-d = VoyagesIO().load_upcoming_flights_from_file()
+# x = AirplanesIO().load_airplanes_from_file()
+# y = AirplanesIO().load_airplanesinfo_from_file()
+# a = CrewIO().load_crew_from_file()
+# b = DestinationsIO().load_destination_from_file()
+# c = VoyagesIO().load_past_flights_from_file()
+# d = VoyagesIO().load_upcoming_flights_from_file()
+
+
+# inputt = 'Cabincrew'
+# x = CrewIO().load_pilot_or_cabincrew(inputt)
+# print(x)
+# a = []
+
+# for crew in x:
+#     b = []
+#     b.append(crew)
+#     a.append(b)
+# print(a)
+
+inputtt = '0'
+y = CrewIO().load_crew_from_file(inputtt)
+print(y)
