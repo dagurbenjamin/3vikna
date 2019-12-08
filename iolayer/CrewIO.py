238da9160580672ce37a5c7693c45806e75a0e9a
indianalifb <Indiana.lif@gmail.com>
2019-12-08 15:52:53 +0000

    Update CrewIO.py

--- iolayer/CrewIO.py
+++ iolayer/CrewIO.py
@@ -6,21 +6,17 @@ class CrewIO():
     def __init__(self, a_str=''):
         self.a_str = a_str
 
-    def load_allcrew_from_file(self):
+    def load_crew_from_file(self):
         allcrew = []
         with open('./data_files/CrewFile.csv') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                employee = Crew(row['ssn'], row['name'], row['role'], row['rank'], row['licence'],
-                                row['address'], row['phonenumber'], row['email'], row['id'])
-                allcrew.append(employee)
-        return allcrew
-
-    def load_onecrew_from_file(self, ssn_in):
-        crewlist = load_allcrew_from_file(self)
-        for c in crewlist
-        if c.get_social() == ssn_in:
-                return c
+                if (row[ssn] == ssn_toFind):
+                    employee = Crew(row['ssn'], row['name'], row['role'], row['rank'], row['licence'],
+                                    row['address'], row['phonenumber'], row['email'], row['id'])
+                    allcrew.append(employee)
+                    break
+        return allcrew  # samt bara 1
 
     def write_in_file(self, new_crew_member=''):
         f = open("CrewFile.csv", "a")
