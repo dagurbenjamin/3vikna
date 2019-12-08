1e66b4893eebd41e16c3397019e6f172dfab960e
ingaldis <58175417+ingaldis@users.noreply.github.com>
2019-12-08 16:38:01 +0000

    prufa á employee

---
+++ test_inga.py
@@ -0,0 +1,10 @@
+import csv
+import os
+from modules.Crew.Crew import Crew
+
+class CrewData(object):
+    def __init__(self):
+        pass
+
+    def check_if_kt_exist(self, kt):
+        data = self.get
