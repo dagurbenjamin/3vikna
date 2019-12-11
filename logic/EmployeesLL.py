08061c9bfff29682c29f4485922437d2a7650cf9
summifu <dagurr19@ru.is>
2019-12-11 13:31:59 +0000

    List all workers not working

--- logic/EmployeesLL.py
+++ logic/EmployeesLL.py
@@ -60,3 +60,22 @@ class EmployeesLL():
     
     def save_new_employee(self, employee):
         CrewIO().write_in_file(employee)
+
+    def employees_not_working(inputt):
+        list_of_non_workers = []
+        list_of_workers = []
+        x = VoyagesIO().load_voyages_from_file(voyageID="0")
+        y = CrewIO().load_crew_from_file(ssn_toFind="0")
+        for line in x:
+            if line.date == inputt:
+                list_of_workers.append(line.captain)
+                list_of_workers.append(line.copilot)
+                list_of_workers.append(line.FlightServiceManager)
+                list_of_workers.append(line.flightAttendant)
+        for line in y:
+            list_of_non_workers.append(line.social)
+        for employee in list_of_non_workers:
+            if employee in list_of_workers:
+                list_of_non_workers.remove(employee)
+
+        return list_of_non_workers
