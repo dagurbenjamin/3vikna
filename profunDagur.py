65c1e2e0e3a1c15522952a0c339cff6a808a83ac
summifu <dagurr19@ru.is>
2019-12-12 18:32:18 +0000

    hata allt

--- profunDagur.py
+++ profunDagur.py
@@ -12,6 +12,17 @@ from logic.DestinationsLL import DestinationsLL
 import csv
 
 
-def get_all_destinations(self):
+def get_all_destinations():
+    x = DestinationsIO().load_destination_from_file(destination_id="0")
+    for line in x:
+        destination = line.destination
+    input_command = ''
+    while input_command != 'q':
+        header = 'All Destinations'
+        print(header)
+        print(destination)
+        if input_command == '1':
+            DestinationsMenu().get_destination('GOH')
 
-     
+
+get_all_destinations()
